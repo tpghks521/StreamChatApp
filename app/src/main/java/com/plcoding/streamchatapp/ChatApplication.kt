b771d8f6cd10fe4519060742457313a73c8e16a2
Alex <alex521@funnc.com>
2021-07-30 16:26:01 +0900

    Chat App

---
+++ app/src/main/java/com/plcoding/streamchatapp/ChatApplication.kt
@@ -0,0 +1,20 @@
+package com.plcoding.streamchatapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import io.getstream.chat.android.client.ChatClient
+import io.getstream.chat.android.offline.ChatDomain
+import javax.inject.Inject
+
+@HiltAndroidApp
+class ChatApplication : Application() {
+
+
+	@Inject
+	lateinit var client: ChatClient
+
+	override fun onCreate() {
+		super.onCreate()
+		ChatDomain.Builder(client, applicationContext).build()
+	}
+}
---
+++ app/src/main/java/com/plcoding/streamchatapp/ui/Constants.kt
@@ -0,0 +1,6 @@
+package com.plcoding.streamchatapp.ui
+
+object Constants {
+
+	const val MIN_USERNAME_LENGTH =3
+}
